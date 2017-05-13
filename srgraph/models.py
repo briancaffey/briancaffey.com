1552250794b52071bfd373d422a23058f678d7a5
mrbrian88 <briancaffey2010@gmail.com>
2017-05-13 12:48:07 -0400

    changed charfield max length

--- srgraph/models.py
+++ srgraph/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Subreddit(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=400, blank=True, null=True)
 
     def __str__(self):
         return str(self.name)
