4f20408f3520282676f806673832f83f5a9b3740
mrbrian88 <briancaffey2010@gmail.com>
2017-05-15 02:18:48 -0400

    added tab-panels for recent popular about and code

--- srgraph/urls.py
+++ srgraph/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import (
     graph_view,
     get_subreddits,
+    popular,
     view_result,
     upvote,
     downvote,
@@ -14,5 +15,6 @@ urlpatterns = [
     url(r'^(?P<id>\d+)/$', view_result, name="view_result"),
     url(r'^(?P<id>\d+)/up/$', upvote, name="upvote"),
     url(r'^(?P<id>\d+)/down/$', downvote, name="downvote"),
+    url(r'^popular/$', popular, name="popular"),
 
 ]
--- srgraph/views.py
+++ srgraph/views.py
@@ -133,6 +133,53 @@ def api_downvote(request, id):
     mimetype = 'appliction/json'
     return HttpResponse(data, mimetype)
 
+def popular(request):
+    sr1 = request.GET.get('sr1')
+    if sr1:
+        sr1 = quote(sr1)
+
+    sr2 = request.GET.get('sr2')
+    if sr2:
+        sr2 = quote(sr2)
+
+    if sr1 and sr2:
+        sr_one = Subreddit.objects.filter(name=sr1 + '\n').first()
+        sr_two = Subreddit.objects.filter(name=sr2 + '\n').first()
+
+        if sr_one and sr_two:
+            search_result = path(sr_one.name.strip('\n'), sr_two.name.strip('\n'))
+            check = SearchResult.objects.filter(s_one=sr_one, s_two=sr_two)
+            new_search_result = SearchResult(s_one=sr_one, s_two=sr_two, path=search_result['path'])
+
+            print("OK1")
+            if len(check) == 0:
+                print("OK")
+                sr = SearchResult(s_one=sr_one, s_two=sr_two, path=search_result['path'])
+                sr.last_searched = datetime.now()
+                sr.save()
+            else:
+                record = check.first()
+                record.last_searched = datetime.now()
+                record.save()
+
+            # context['new_search_result'] = search_result
+
+    previous_searches = SearchResult.objects.all().order_by('-votes')
+    # context['previous_searches'] = previous_searches
+    # numbers_list = range(1, 1000)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(previous_searches, 20)
+    try:
+        results = paginator.page(page)
+    except PageNotAnInteger:
+        results = paginator.page(1)
+    except EmptyPage:
+        results = paginator.page(paginator.num_pages)
+
+    # context['numbers'] = numbers
+
+    # context['previous_searches'] = SearchResult.objects.all()
+    return render(request, 'srgraph/srgraph_infinite_scroll_popular.html', {'results':results})
 
 #
 # def get_random(request):
