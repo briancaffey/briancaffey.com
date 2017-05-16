d19a14705343c9390c5bfd1c05c59ca93b41c68e
mrbrian88 <briancaffey2010@gmail.com>
2017-05-16 02:41:23 -0400

    changed project ordering

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,6 @@ from .models import Project
 
 # Create your views here.
 def projects_home(request):
-	projects = Project.objects.all()
+	projects = Project.objects.all().order_by('order')
 	context = {'projects':projects}
 	return render(request, 'projects/projects_home.html', context)
