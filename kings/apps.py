dc60a8656c542b40ecd40fa244981b3bed1b43c0
mrbrian88 <briancaffey2010@gmail.com>
2017-05-30 16:04:03 -0400

    javascript game updated

---
+++ kings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KingsConfig(AppConfig):
+    name = 'kings'
--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from projects import views
 
 urlpatterns = [
 		url(r'^$', views.projects_home, name='projects'),
 		url(r'^bokeh/$', views.bokeh, name='bokeh'),
 		url(r'^kingsencounter/$', views.kings, name='kings'),
+
 ]
