b6a133befed6b3b1a652dd232fd278cae8a095c2
mrbrian88 <briancaffey2010@gmail.com>
2017-06-14 18:23:55 -0400

    added admin link for client demo

--- yawlih/urls.py
+++ yawlih/urls.py
@@ -9,6 +9,7 @@ from .views import (
 
 
 urlpatterns = [
+    
     url(r'^search/$', advanced_search, name="advanced_search"),
     url(r'^$', yl_home, name="home"),
     url(r'^logout/$', user_logout, name="user_logout"),
