f0c6dfb8761dbda54be3fd34a724b4b33c24f44e
mrbrian88 <briancaffey2010@gmail.com>
2017-07-30 03:58:27 -0400

    removed id from data in ajax

--- playquest/urls.py
+++ playquest/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
 		url(r'^create/$', views.create, name='create'),
 		url(r'^demo/$', views.demo, name='demo'),
 		url(r'^$', views.playquest_home, name="home"),
-		url(r'^game/(?P<id>\d+)/$', views.game_id, name='game_id'),
+		url(r'^game/(?P<id>[A-Za-z0-9]+)/$', views.game_id, name='game_id'),
 		url(r'^json/$', views.sample_json, name="sample_json"),
 		## To Do
-		url(r'^game/(?P<id>\id+)/edit/$', views.edit_game, name="edit"),
+		url(r'^game/(?P<id>[A-Za-z0-9]+)/edit/$', views.edit_game, name="edit"),
 
 		# API CRU[D] endpoints coming from /api/kings/ (in main urls.py)
 		url(r'^(?P<pk>\d+)/update/$', GameUpdateAPIView.as_view(), name="update-game"),
