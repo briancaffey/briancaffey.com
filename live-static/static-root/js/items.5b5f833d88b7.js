deeea59b2ceedfb86f61cd296c962a0b91234522
mrbrian88 <briancaffey2010@gmail.com>
2017-09-04 12:53:09 -0400

    added text to speach to read dialogue

---
+++ live-static/static-root/js/items.5b5f833d88b7.js
@@ -0,0 +1,985 @@
+var things = {
+
+    'blank':{
+      'emoji': ' ',
+      'wall':false,
+      'name':'blank',
+    },
+
+
+    'tent': {
+      'emoji': '🎪',
+      'wall': true,
+      'name':'tent',
+    },
+
+    'tree': {
+    'emoji': '🌳',
+    'wall': true,
+    'name':'tree',
+    },
+
+
+    'heart' : {
+     'emoji': '❤️',
+     'wall': false,
+     'name':  'heart',
+     'item': true,
+   },
+
+    'diamond' : {
+     'emoji': '💎',
+     'wall': false,
+     'name':'diamond',
+     'item': true,
+   },
+
+
+  'player': {
+    'emoji': '👱🏼',
+    'wall': true,
+    'name':'player',
+    'current':[1,2],
+  },
+
+
+   'scroll' : {
+    'emoji': '📜',
+    'wall': false,
+    'name': 'scroll',
+  },
+
+
+
+   'orb' : {
+    'emoji': '🔮',
+    'name':'orb',
+  },
+
+   'castle' : {
+    'emoji': '🏰',
+    'wall':true,
+    'exitway':true,
+    'name':'castle',
+  },
+
+   'sunflower' : {
+    'emoji': '🌻',
+    'wall': true,
+    'name':'sunflower',
+  },
+
+   'flower' : {
+    'emoji': '🌷',
+    'wall':true,
+    'name':'flower',
+  },
+
+    'squirrle' : {
+    'emoji': '🐿',
+    'wall': true,
+    'name':'squirrle',
+  },
+
+    'owl' : {
+    'emoji': '🦉',
+    'wall':true,
+    'name':'owl',
+  },
+
+    'arms' : {
+    'emoji': '🛡',
+    'wall':true,
+    'name':'arms',
+  },
+
+    'candle' : {
+    'emoji': '🕯',
+    'wall':true,
+    'name':'candle',
+  },
+
+    'square_diamond' : {
+    'emoji': '💠',
+    'wall': true,
+    'name':'square_diamond',
+  },
+
+    'spd' : {
+    'emoji': '🔹',
+    'name':'spd'
+  },
+
+    'clb' : {
+    'emoji': '🔶',
+    'name':'clb'
+  },
+
+
+    'dmnd' : {
+    'emoji': '🔸',
+    'name':'dmnd'
+  },
+
+    'hrt' : {
+    'emoji': '🔷',
+    'name':'hrt'
+  },
+
+    'crown' : {
+    'emoji': '🤴🏻',
+    'wall': true,
+    'name':'crown',
+  },
+
+    'horn' : {
+    'emoji': '📯',
+    'wall': true,
+    'name':'horn'
+  },
+
+    'guard' : {
+    'emoji': '💂🏻',
+    'wall':true,
+    'name':'guard',
+  },
+
+    'door' : {
+    'emoji': '🚪',
+    'name':'door',
+
+  },
+
+    'painting' : {
+    'emoji': '🖼',
+    'wall': true,
+    'name':'painting',
+  },
+
+    'festive' : {
+    'emoji': '🏵',
+    'wall':true,
+    'name':'festive',
+  },
+
+    'cheese' : {
+    'emoji': '🧀',
+    'wall': true,
+    'name':'cheese',
+  },
+
+    'fountain' : {
+    'emoji': '⛲',
+    'wall': true,
+    'name':'foundation',
+  },
+
+    'key' : {
+    'emoji':'🗝',
+    'name':'key',
+    'item': true,
+  },
+
+    'woman' : {
+    'emoji': '👸🏼',
+    'wall': true,
+    'name':'woman',
+  },
+
+    'sword' : {
+    'emoji': '🗡',
+    'name':'sword',
+
+  },
+
+    'card' : {
+    'emoji': '🃏',
+    'name':'card',
+  },
+
+    'snail' : {
+    'emoji': '🐌',
+    'name':'snail',
+  },
+
+    'mouse' : {
+    'emoji': '🐭',
+    'wall': true,
+    'name':'mouse',
+  },
+
+    'swords' : {
+    'emoji': '⚔️',
+    'wall': true,
+    'name':'swords',
+  },
+
+    'nut' : {
+    'emoji': '🌰',
+    'name':'nut',
+  },
+
+    'beer' : {
+    'emoji': '🍺',
+    'wall': true,
+    'name':'beer',
+  },
+
+    'grapes' : {
+    'emoji': '🍇',
+    'wall': true,
+    'name':'grapes',
+
+  },
+
+    'meat' : {
+    'emoji': '🍗',
+    'wall': true,
+    'name':'meat',
+  },
+
+    'apple' : {
+    'emoji': '🍎',
+    'wall': true,
+    'name':'apple',
+  },
+
+    'clown' : {
+    'emoji': '🤡',
+    'wall': true,
+    'name':'clown',
+  },
+
+    'bread' : {
+    'emoji': '🥖',
+    'wall': true,
+    'name':'bread',
+  },
+
+    'pan' : {
+    'emoji': '🥘',
+    'wall': true,
+    'name':'pan',
+  },
+
+    'jester' : {
+    'emoji': '🤹‍',
+    'wall':true,
+    'name':'jester',
+
+  },
+
+    'mother' : {
+    'emoji': '🤶🏻',
+    'wall': true,
+    'name':'mother',
+  },
+
+    'fleur' : {
+    'emoji': '⚜️',
+    'wall': true,
+    'name':'fleur',
+  },
+
+    'study' : {
+    'emoji': '🔰',
+    'wall': true,
+    'name':'study',
+  },
+
+    'bird' : {
+    'emoji': '🐦',
+    'wall': true,
+    'name':'bird',
+  },
+
+    'wine' : {
+    'emoji': '🍷',
+    'wall':true,
+    'name':'wine',
+  },
+
+    'pear' : {
+    'emoji': '🍐',
+    'wall': true,
+    'name':'pear',
+  },
+
+    'orange' : {
+    'emoji': '🍊',
+    'name':'orange',
+  },
+
+    'plate' : {
+    'emoji': '🍽',
+    'wall': true,
+    'name':'plate',
+  },
+
+
+    'man' : {
+    'emoji': '👱🏼',
+    'wall': true,
+    'name':'man',
+  },
+
+    'court' : {
+    'emoji': '🏛',
+    'wall': true,
+    'name':'court',
+  },
+
+    'building' : {
+    'emoji': '🏤',
+    'wall': true,
+    'name':'building',
+  },
+
+    'tower' : {
+    'emoji': '🏫',
+    'wall': true,
+    'name':'tower',
+
+  },
+
+    'house' : {
+    'emoji': '🏡',
+    'wall':true,
+    'name':'house',
+  },
+
+    'judge' : {
+    'emoji': '👨🏻‍⚖️',
+    'wall': true,
+    'name':'judge',
+  },
+
+    'lady' : {
+    'emoji': '👩🏼',
+    'wall': true,
+    'lady':'name',
+  },
+    'up_down' : {
+    'emoji': '↕️',
+    'exitway': true,
+    'name':'up_down',
+  },
+
+    'lock' : {
+    'emoji': '🔒',
+    'wall': true,
+    'name':'lock',
+  },
+
+    'flag' : {
+    'emoji': '🇧🇹',
+    'wall':true,
+    'name':'flag',
+  },
+
+    'rainbow' : {
+    'emoji': '🌈',
+    'wall': true,
+    'name':'rainbow',
+  },
+
+    'trident' : {
+    'emoji': '🔱',
+    'wall': true,
+    'name':'trident',
+  },
+
+    'mustache' : {
+    'emoji': '👨🏻',
+    'wall': true,
+    'name':'mustache',
+  },
+
+    'blonde_girl' : {
+    'emoji': '👧🏼',
+    'wall': true,
+    'name':'blonde_girl',
+  },
+
+    'books1' : {
+    'emoji': '📕',
+    'wall': true,
+    'name':'books1',
+  },
+
+    'books2' : {
+    'emoji': '📗',
+    'wall': true,
+    'name':'books2',
+  },
+
+    'books3' : {
+    'emoji': '📘',
+    'wall': true,
+    'name':'books3',
+  },
+
+    'books4' : {
+    'emoji': '📙',
+    'wall': true,
+    'name':'books4',
+  },
+
+    'books5' : {
+    'emoji': '📔',
+    'wall': true,
+    'name':'books5',
+  },
+
+    'books6' : {
+    'emoji': '📓',
+    'wall': true,
+    'name':'books6',
+  },
+
+    'beaker' : {
+    'emoji': '⚗️',
+    'wall': true,
+    'name':'beaker',
+  },
+
+    'agri' : {
+    'emoji': '🌾',
+    'wall': true,
+    'name':'agri',
+  },
+
+    'world' : {
+    'emoji': '🌍',
+    'wall': true,
+    'name':'world',
+  },
+
+    'books7' : {
+    'emoji': '📖',
+    'wall': true,
+    'name':'books7',
+  },
+
+    'books0' : {
+    'emoji': '📚',
+    'wall': true,
+    'name':'books0',
+  },
+
+
+    'dove' : {
+    'emoji': '🕊',
+    'wall': true,
+    'name':'dove',
+  },
+
+   'church' : {
+    'emoji': '⛪️',
+    'wall': true,
+    'name':'church',
+  },
+
+}
+
+// var emoji_map =[[things.player, things.tree, things.tent, things.blank, things.heart,things.diamond,things.scroll,things.orb,things.castle,things.sunflower,things.flower,things.squirrle,things.owl,things.arms,things.candle,things.square_diamond,],
+//                 [things.spd,things.clb,things.dmnd,things.hrt,things.crown,things.horn,things.guard,things.door,things.painting,things.festive,things.cheese,things.fountain,things.key,things.woman,things.sword,things.card,],
+//                 [things.snail,things.mouse,things.swords,things.nut,things.beer,things.grapes,things.meat,things.apple,things.clown,things.bread,things.pan,things.jester,things.mother,things.fleur,things.study,things.bird,],
+//                 [things.wine,things.pear,things.orange,things.plate,things.man,things.court,things.tower,things.house,things.judge,things.lady,things.lock,things.up_down,things.flag,things.rainbow,things.trident,things.mustache,],
+//                 [things.books2,things.books3,things.books4,things.books5,things.books6,things.beaker,things.agri,things.world,things.books7,things.books0,things.dove,things.church,things.blonde_girl,things.books1,],
+//               ]
+
+
+emoji_map =  [
+  [
+
+    {
+        'emoji': ' ',
+        'wall':false,
+        'name':'blank',
+      },
+
+
+{
+  'emoji': '🌳',
+  'wall': true,
+  'name':'tree',
+  },
+
+  {
+     'emoji': '🚪',
+     'name':'door',
+
+   },
+
+
+  {
+      'emoji': '🎪',
+      'wall': true,
+      'name':'tent',
+    },
+
+
+
+{
+    'emoji': '❤️',
+    'wall': false,
+    'name':  'heart',
+    'item': true,
+  },
+
+{
+    'emoji': '💎',
+    'wall': false,
+    'name':'diamond',
+    'item':true,
+  },
+
+  {
+      'emoji': '🔮',
+      'name':'orb',
+    },
+
+
+{
+    'emoji': '📜',
+    'wall': false,
+    'name': 'scroll',
+  },
+
+  {
+      'emoji': '👱🏼',
+      'wall': true,
+      'name':'player',
+      'current':[1,2],
+    },
+
+
+
+ {
+    'emoji': '🏰',
+    'wall':true,
+    'exitway':true,
+    'name':'castle',
+  },
+
+ {
+    'emoji': '🌻',
+    'wall': true,
+    'name':'sunflower',
+  },
+
+{
+    'emoji': '🌷',
+    'wall':true,
+    'name':'flower',
+  },
+
+{
+    'emoji': '🐿',
+    'wall': true,
+    'name':'squirrle',
+  },
+
+ {
+    'emoji': '🛡',
+    'wall':true,
+    'name':'arms',
+  },
+
+ {
+    'emoji': '🕯',
+    'wall':true,
+    'name':'candle',
+  },
+
+  {
+    'emoji': '💠',
+    'wall': true,
+    'name':'square_diamond',
+  },
+],
+[
+
+{
+    'emoji': '🔹',
+    'name':'spd'
+  },
+
+ {
+    'emoji': '🔶',
+    'name':'clb'
+  },
+
+
+{
+    'emoji': '🔸',
+    'name':'dmnd'
+  },
+
+ {
+    'emoji': '🔷',
+    'name':'hrt'
+  },
+
+ {
+    'emoji': '🤴🏻',
+    'wall': true,
+    'name':'crown',
+  },
+
+ {
+    'emoji': '📯',
+    'wall': true,
+    'name':'horn'
+  },
+
+ {
+    'emoji': '💂🏻',
+    'wall':true,
+    'name':'guard',
+  },
+
+  {
+     'emoji': '🦉',
+     'wall':true,
+     'name':'owl',
+   },
+
+ {
+    'emoji': '🖼',
+    'wall': true,
+    'name':'painting',
+  },
+
+ {
+    'emoji': '🏵',
+    'wall':true,
+    'name':'festive',
+  },
+
+ {
+    'emoji': '🧀',
+    'wall': true,
+    'name':'cheese',
+  },
+
+ {
+    'emoji': '⛲',
+    'wall': true,
+    'name':'foundation',
+  },
+
+ {
+    'emoji':'🗝',
+    'name':'key',
+    'item': true,
+  },
+
+ {
+    'emoji': '👸🏼',
+    'wall': true,
+    'name':'woman',
+  },
+
+ {
+    'emoji': '🗡',
+    'name':'sword',
+
+  },
+
+ {
+    'emoji': '🃏',
+    'name':'card',
+  },
+],
+[
+
+ {
+    'emoji': '🐌',
+    'name':'snail',
+  },
+
+ {
+    'emoji': '🐭',
+    'wall': true,
+    'name':'mouse',
+  },
+
+ {
+    'emoji': '⚔️',
+    'wall': true,
+    'name':'swords',
+  },
+
+{
+    'emoji': '🌰',
+    'name':'nut',
+  },
+
+ {
+    'emoji': '🍺',
+    'wall': true,
+    'name':'beer',
+  },
+
+ {
+    'emoji': '🍇',
+    'wall': true,
+    'name':'grapes',
+
+  },
+
+{
+    'emoji': '🍗',
+    'wall': true,
+    'name':'meat',
+  },
+
+ {
+    'emoji': '🍎',
+    'wall': true,
+    'name':'apple',
+  },
+
+ {
+    'emoji': '🤡',
+    'wall': true,
+    'name':'clown',
+  },
+
+ {
+    'emoji': '🥖',
+    'wall': true,
+    'name':'bread',
+  },
+
+ {
+    'emoji': '🥘',
+    'wall': true,
+    'name':'pan',
+  },
+
+ {
+    'emoji': '🤹‍',
+    'wall':true,
+    'name':'jester',
+
+  },
+
+   {
+    'emoji': '🤶🏻',
+    'wall': true,
+    'name':'mother',
+  },
+
+ {
+    'emoji': '⚜️',
+    'wall': true,
+    'name':'fleur',
+  },
+
+ {
+    'emoji': '🔰',
+    'wall': true,
+    'name':'study',
+  },
+
+ {
+    'emoji': '🐦',
+    'wall': true,
+    'name':'bird',
+  },
+],
+[
+
+{
+    'emoji': '🍷',
+    'wall':true,
+    'name':'wine',
+  },
+
+ {
+    'emoji': '🍐',
+    'wall': true,
+    'name':'pear',
+  },
+
+ {
+    'emoji': '🍊',
+    'name':'orange',
+  },
+
+ {
+    'emoji': '🍽',
+    'wall': true,
+    'name':'plate',
+  },
+
+
+ {
+    'emoji': '👱🏼',
+    'wall': true,
+    'name':'man',
+  },
+
+ {
+    'emoji': '🏛',
+    'wall': true,
+    'name':'court',
+  },
+
+   {
+    'emoji': '🏤',
+    'wall': true,
+    'name':'building',
+  },
+
+ {
+    'emoji': '🏫',
+    'wall': true,
+    'name':'tower',
+
+  },
+
+ {
+    'emoji': '🏡',
+    'wall':true,
+    'name':'house',
+  },
+
+{
+    'emoji': '👨🏻‍⚖️',
+    'wall': true,
+    'name':'judge',
+  },
+
+{
+    'emoji': '👩🏼',
+    'wall': true,
+    'lady':'name',
+  },
+ {
+    'emoji': '↕️',
+    'exitway': true,
+    'name':'up_down',
+  },
+
+{
+    'emoji': '🔒',
+    'wall': true,
+    'name':'lock',
+  },
+
+{
+    'emoji': '🇧🇹',
+    'wall':true,
+    'name':'flag',
+  },
+
+ {
+    'emoji': '🌈',
+    'wall': true,
+    'name':'rainbow',
+  },
+
+ {
+    'emoji': '🔱',
+    'wall': true,
+    'name':'trident',
+  },
+],
+[
+{
+    'emoji': '👨🏻',
+    'wall': true,
+    'name':'mustache',
+  },
+
+ {
+    'emoji': '👧🏼',
+    'wall': true,
+    'name':'blonde_girl',
+  },
+
+ {
+    'emoji': '📕',
+    'wall': true,
+    'name':'books1',
+  },
+
+ {
+    'emoji': '📗',
+    'wall': true,
+    'name':'books2',
+  },
+
+{
+    'emoji': '📘',
+    'wall': true,
+    'name':'books3',
+  },
+
+ {
+    'emoji': '📙',
+    'wall': true,
+    'name':'books4',
+  },
+
+ {
+    'emoji': '📔',
+    'wall': true,
+    'name':'books5',
+  },
+
+{
+    'emoji': '📓',
+    'wall': true,
+    'name':'books6',
+  },
+
+ {
+    'emoji': '⚗️',
+    'wall': true,
+    'name':'beaker',
+  },
+
+{
+    'emoji': '🌾',
+    'wall': true,
+    'name':'agri',
+  },
+
+ {
+    'emoji': '🌍',
+    'wall': true,
+    'name':'world',
+  },
+
+{
+    'emoji': '📖',
+    'wall': true,
+    'name':'books7',
+  },
+
+ {
+    'emoji': '📚',
+    'wall': true,
+    'name':'books0',
+  },
+
+
+{
+    'emoji': '🕊',
+    'wall': true,
+    'name':'dove',
+  },
+
+ {
+    'emoji': '⛪️',
+    'wall': true,
+    'name':'church',
+  },
+]
+
+]
